1d2aaf52d87142f1e42394ad6d32ddf591618adf
Benjamin Hole <bhole@jaguarlandrover.com>
2023-12-19 16:48:17 +0000

    Header framework has been creeated and partially stylised

--- app.py
+++ app.py
@@ -7,6 +7,40 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/accountSignIn')
+def account_sign_in():
+    return render_template('index.html')
+
+@app.route('/basket')
+def basket():
+    return render_template('index.html')
+
+
+
+@app.route('/carparts')
+def car_parts():
+    return "CARPARTS"
+
+@app.route('/animals')
+def animals():
+    return "ANIMALS"
+
+@app.route('/sports')
+def sports():
+    return "SPORTS"
+
+@app.route('/books')
+def books():
+    return "BOOKS"
+
+@app.route('/phones')
+def phones():
+    return "PHONES"
+
+@app.route('/music')
+def music():
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)

1ea7b40771a9bc8960091d8fc47b705f7eec0657
Benjamin Hole <bhole@jaguarlandrover.com>
2023-12-21 12:34:04 +0000

    Started framework for product bar and cards

---
+++ productBar.js
@@ -0,0 +1,26 @@
+const template = document.createElement('template');
+
+number = 1
+
+template.innerHTML = `
+    <div class="productCard">
+        <p>{ number }</p>
+    </div>
+`;
+
+
+document.body.appendChild(template.content)
+
+function displayProducts(products) {
+    const container = document.getElementById('productBar');
+
+    products.foreach(product => {
+        const card = document.createElement('div');
+        card.classList.add('productCard');
+
+        card.innerHTML = `
+            <img class="productImg" src="${product.image}" alt="${product.title}
+        `;
+        
+    })
+}

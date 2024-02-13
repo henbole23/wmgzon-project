43b7eee31ea382e785bab6110d5d8973fc2ee029
Benjamin Hole <bhole@jaguarlandrover.com>
2024-02-13 19:31:29 +0000

    database table frameworks made and setup

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
 from flask_bcrypt import Bcrypt
 from datetime import datetime
 from forms import *
@@ -41,12 +41,29 @@ class Users(db.Model, UserMixin):
 
     def get_id(self):
            return (self.user_id)
+    
+artist_album = db.Table('ARTISTALBUM',
+                          db.Column('artist_id', db.Integer, db.ForeignKey('ARTISTS.artist_id')),
+                          db.Column('album_id', db.Integer, db.ForeignKey('ALBUMS.album_id'))
+                          )
+
+artist_song = db.Table('ARTISTSONG',
+                          db.Column('artist_id', db.Integer, db.ForeignKey('ARTISTS.artist_id')),
+                          db.Column('song_id', db.Integer, db.ForeignKey('SONGS.song_id'))
+                          )
+
+album_song = db.Table('ALBUMSONG',
+                          db.Column('album_id', db.Integer, db.ForeignKey('ALBUMS.album_id'), primary_key=True),
+                          db.Column('song_id', db.Integer, db.ForeignKey('SONGS.song_id'), primary_key=True)
+                          )
 
 class Artists(db.Model):
     __tablename__ = "ARTISTS"
     artist_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     bio = db.Column(db.String(500))
+    artist_albums = db.relationship('Albums', secondary=artist_album, back_populates='album_artists')
+    artist_songs = db.relationship('Songs', secondary=artist_song, back_populates='song_artists')
 
 class Albums(db.Model):
    __tablename__ = "ALBUMS"
@@ -55,31 +72,51 @@ class Albums(db.Model):
    artwork = db.Column(db.String(100), nullable=False)
    genre = db.Column(db.String(25))
    year = db.Column(db.Integer, nullable=False)
-   fk_artist_id = db.Column(db.Integer, db.ForeignKey("ARTISTS.artist_id"), nullable=False)
+   album_artists = db.relationship('Artists', secondary=artist_album, back_populates='artist_albums')
+   album_songs = db.relationship('Songs', secondary=album_song, back_populates='song_albums')
 
 class Songs(db.Model):
    __tablename__ = "SONGS"
    song_id = db.Column(db.Integer, primary_key=True)
    name = db.Column(db.String(100), nullable=False)
    length = db.Column(db.Integer, nullable=False)
-   fk_album_id = db.Column(db.Integer, db.ForeignKey("ALBUMS.album_id"), nullable=False)
-   fk_artist_id = db.Column(db.Integer, db.ForeignKey("ARTISTS.artist_id"), nullable=False)
+   song_artists = db.relationship('Artists', secondary=artist_song, back_populates='artist_songs')
+   song_albums = db.relationship('Albums', secondary=album_song, back_populates='album_songs')
+#    fk_album_id = db.Column(db.Integer, db.ForeignKey("ALBUMS.album_id"), nullable=False)
 
+class Format(db.Model):
+    __tablename__ = "FORMATS"
+    format_id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(50), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+
+class Order(db.Model):
+    __tablename__ = "ORDERS"
+    order_id = db.Column(db.Integer, primary_key=True)
+    fk_user_id = db.Column(db.Integer, db.ForeignKey("USERS.user_id"), nullable=False)
+    order_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+class OrderItem(db.Model):
+    __tablename__ = "ORDERITEMS"
+    orderitem_id = db.Column(db.Integer, primary_key=True)
+    fk_order_id = db.Column(db.Integer, db.ForeignKey("ORDERS.order_id"), nullable=False)
+    fk_album_id = db.Column(db.Integer, db.ForeignKey("ALBUMS.album_id"), nullable=False)
+    fk_format_id = db.Column(db.Integer, db.ForeignKey("FORMATS.format_id"), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+
+# def get_all_products():
+#     db = sqlite3.connect("wmgzon.db")
+#     cursor = db.cursor()
+
+#     fetch_albums = """SELECT ARTISTS.artist_id, ARTISTS.artist_name, ALBUMS.album_name, albums.artwork FROM ARTISTS
+#                       INNER JOIN ALBUMS ON ARTISTS.artist_id=ALBUMS.fk_artist_id"""
+
+#     cursor.execute(fetch_albums)
+#     products = cursor.fetchall()
     
+#     db.close()
 
-def get_all_products():
-    db = sqlite3.connect("wmgzon.db")
-    cursor = db.cursor()
-
-    fetch_albums = """SELECT ARTISTS.artist_id, ARTISTS.artist_name, ALBUMS.album_name, albums.artwork FROM ARTISTS
-                      INNER JOIN ALBUMS ON ARTISTS.artist_id=ALBUMS.fk_artist_id"""
-
-    cursor.execute(fetch_albums)
-    products = cursor.fetchall()
-    
-    db.close()
-
-    return products
+#     return products
 
 def get_user_details(user, password):
     with sqlite3.connect("wmgzon.db") as db:
@@ -93,13 +130,17 @@ def get_user_details(user, password):
 
 @app.route('/')
 def index():
-    return render_template('index.html', products=get_all_products())
+    # albums = db.session.query(db.select(Albums)).all()
+    albums = Albums.query.all()
+    print(albums)
+    return render_template('index.html', products=albums)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     register_form = RegisterForm()
     if request.method == 'POST' and register_form.validate_on_submit():
         user = Users.query.filter_by(email=request.form['email']).first()
+        # user = db.session.scalars(db.select(Users).filter_by(email=request.form['email'])).first()
         if user is None:
             username = request.form['username']
             user = Users(username=username,
@@ -205,4 +246,7 @@ def music():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+    
+
+
     app.run(debug=True)
